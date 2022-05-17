c3b1cba6f5034b86412a6d8a94f8ab4adab6c6e9
zamee <zamee0192@gmail.com>
2022-05-18 04:08:08 +0600

    colors added

---
+++ app/src/main/java/com/hussain/fluttercommitlist/util/Constant.java
@@ -0,0 +1,8 @@
+package com.hussain.fluttercommitlist.util;
+
+import com.hussain.fluttercommitlist.R;
+
+public class Constant {
+    public static String base_url = "";
+    public static int repo_name = R.string.repo_name;
+}
--- app/src/main/java/com/hussain/fluttercommitlist/view/MainActivity.kt
+++ app/src/main/java/com/hussain/fluttercommitlist/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hussain.fluttercommitlist
+package com.hussain.fluttercommitlist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hussain.fluttercommitlist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
